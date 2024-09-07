9ca29d87f357209f43f061e368120ad7ab527018
Nehemiah Negussie <46787242+nehemiah-negussie@users.noreply.github.com>
2024-09-07 02:12:38 -0400

    hello world gradient

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let height = 100;
+    let width = 200;
+    print!("P3\n{} {}\n255\n", width, height);
+    for j in 0..height {
+        for i in 0..width {
+            let r: f64 = i as f64 / (width-1) as f64;
+            let g: f64 = j as f64 / (height-1) as f64;
+            let b: f64 = 0.0;
+            print!("{} {} {}\n", (r * 255.0) as i32, (g * 255.0) as i32, (b * 255.0) as i32);
+        }
+    }
 }
